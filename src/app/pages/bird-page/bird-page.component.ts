54f1d3b8ae026d5e939750bad362199389690d38
Stefan <stefken147@hotmail.com>
2018-11-18 17:10:58 +0000

    Adding blog page

--- src/app/pages/bird-page/bird-page.component.ts
+++ src/app/pages/bird-page/bird-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {fadeInAnimation} from "../../app.animations";
+import {ContentService} from "../../shared/content.service";
 
 @Component({
-  selector: 'app-bird-page',
+  selector: 'bird',
   templateUrl: './bird-page.component.html',
   styleUrls: ['./bird-page.component.less']
+  animations: [fadeInAnimation],
+  host: { '[@fadeInAnimation]': '' }
 })
 export class BirdPageComponent implements OnInit {
+  pageContent: any;
+  constructor(private contentService: ContentService) {};
 
-  constructor() { }
 
   ngOnInit() {
-  }
+    this.contentService.getPages().subscribe(data => {
+      data.forEach(page => {
+        if(page.slug === 'birdtable'){
+          this.pageContent = page.content.rendered;
+        };
+      });
+    }, error => console.log('Could not load Page Content'));
+  };
 
 }
--- src/app/pages/blog-page/blog-page.component.ts
+++ src/app/pages/blog-page/blog-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {fadeInAnimation} from "../../app.animations";
+import {ContentService} from "../../shared/content.service";
 
 @Component({
-  selector: 'app-blog-page',
+  selector: 'blog',
   templateUrl: './blog-page.component.html',
   styleUrls: ['./blog-page.component.less']
+  animations: [fadeInAnimation],
+  host: { '[@fadeInAnimation]': '' }
 })
-export class BlogPageComponent implements OnInit {
 
-  constructor() { }
+export class BlogPageComponent implements OnInit {
+  postsContent: any;
+
+  constructor(private contentService: ContentService) {};
 
   ngOnInit() {
+    this.contentService.getPosts().subscribe(data => {
+      this.postsContent = data;
+    }, error => console.log('Could not load Page Content'));
+  };
+
+  getAuthor(authorId) {
+    var author = 'David Spence';
+    if(authorId === 2){
+      author = 'David Spence';
+    }
+    return author;
   }
 
 }
